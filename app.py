7b87b61ddde86471e7d221c772cc8b09e77039d0
lederman <ledermannoam18@gmail.com>
2023-02-04 23:29:56 +0200

    2 commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import flask
 
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8085)(debug=True)
